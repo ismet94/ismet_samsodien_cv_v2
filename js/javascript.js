99d54525ed8b4e6d6f38e6fe4f8a88c08274122c
ismet94 <ismetsamso94@gmail.com>
2019-04-10 14:37:30 +0200

    Added additional projects

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,24 @@
+// This controls the effects timing
 AOS.init({
     duration: 2800,
   })
   
+
+// Back to top script
+$(window).scroll(function() {
+  // Once scrolled pass 60px then the arrow appears
+  if ($(this).scrollTop() >= 50) {     
+     // The arrow fades in 
+      $('#return-to-top').fadeIn(200);   
+  } else {
+    // Else the arrow would fade out
+      $('#return-to-top').fadeOut(200);   
+  }
+});
+// When clicked the function would return to top
+$('#return-to-top').click(function() {      
+  $('body,html').animate({
+     // Scroll back to the top of the body
+      scrollTop : 0                      
+  }, 500);
+});
